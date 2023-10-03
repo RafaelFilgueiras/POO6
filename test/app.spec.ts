ec25a305f934cba593b21f5a873cfd422e7fea17
RafaelFeijo <feijo.rafael@unifesp.br>
2023-10-02 23:43:22 -0300

    ex8

---
+++ test/app.spec.ts
@@ -0,0 +1,92 @@
+import sinon from "sinon"
+import { App } from "./app"
+import { Bike } from "./bike"
+import { User } from "./user"
+import { Location } from "./location"
+import { BikeNotFoundError } from "./errors/bike-not-found-error"
+import { UnavailableBikeError } from "./errors/unavailable-bike-error"
+import { UserNotFoundError } from "./errors/user-not-found-error"
+
+describe('App', () => {
+    it('should correctly calculate the rent amount', async () => {
+        const app = new App()
+        const user = new User('Jose', 'jose@mail.com', '1234')
+        await app.registerUser(user)
+        const bike = new Bike('caloi mountainbike', 'mountain bike',
+            1234, 1234, 100.0, 'My bike', 5, [])
+        app.registerBike(bike)
+        const clock = sinon.useFakeTimers();
+        app.rentBike(bike.id, user.email)
+        const hour = 1000 * 60 * 60
+        clock.tick(2 * hour)
+        const rentAmount = app.returnBike(bike.id, user.email)
+        expect(rentAmount).toEqual(200.0)
+    })
+
+    it('should be able to move a bike to a specific location', () => {
+        const app = new App()
+        const bike = new Bike('caloi mountainbike', 'mountain bike',
+            1234, 1234, 100.0, 'My bike', 5, [])
+        app.registerBike(bike)
+        const newYork = new Location(40.753056, -73.983056)
+        app.moveBikeTo(bike.id, newYork)
+        expect(bike.location.latitude).toEqual(newYork.latitude)
+        expect(bike.location.longitude).toEqual(newYork.longitude)
+    })
+
+    it('should throw bike not found error when trying to move an unregistered bike', () => {
+        const app = new App()
+        const newYork = new Location(40.753056, -73.983056)
+        expect(() => {
+            app.moveBikeTo('fake-id', newYork)
+        }).toThrow(BikeNotFoundError)
+    })
+
+    it('should correctly handle bike rent', async () => {
+        const app = new App()
+        const user = new User('Jose', 'jose@mail.com', '1234')
+        await app.registerUser(user)
+        const bike = new Bike('caloi mountainbike', 'mountain bike',
+            1234, 1234, 100.0, 'My bike', 5, [])
+        app.registerBike(bike)
+        app.rentBike(bike.id, user.email)
+        expect(app.rents.length).toEqual(1)
+        expect(app.rents[0].bike.id).toEqual(bike.id)
+        expect(app.rents[0].user.id).toEqual(user.id)
+    })
+
+    it('should throw unavaiable bike error when trying to rent unavailable bike', async () => {
+        const app = new App()
+        const user = new User('Jose', 'jose@mail.com', '1234')
+        await app.registerUser(user)
+        const bike = new Bike('caloi mountainbike', 'mountain bike',
+            1234, 1234, 100.0, 'My bike', 5, [])
+        app.registerBike(bike)
+        app.rentBike(bike.id, user.email)
+        expect(() => {
+            app.rentBike(bike.id, user.email)
+        }).toThrow(UnavailableBikeError)
+    })
+    
+    it('should throw user not found error when trying to authentication'), async () => {
+        const app = new App()
+        expect(() => {app.authenticate('jos@email.com', '1234')}).toThrow(UserNotFoundError)
+    }
+
+    it('should correctly handle register user'), async () => {
+        const app = new App()
+        const user = new User('Jose', 'jose@mail.com', '1234')
+        app.registerUser(user)
+        expect(app.users.length).toEqual(1)
+        expect(app.users[0].email).toEqual('jose@email.com')
+        expect(app.users[0].name).toEqual('jose')
+    }
+
+    it('should correctly remove register user'), async () => {
+        const app = new App()
+        const user = new User('Jose', 'jose@mail.com', '1234')
+        app.registerUser(user)
+        app.removeUser('jose@email.com')
+        expect(app.users.length).toEqual(0)
+    }
+})
